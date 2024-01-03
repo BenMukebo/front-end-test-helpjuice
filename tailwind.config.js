5c7fe12bb9f5ecbd7e539c22604526d907e1263c
BenMukebo <l.benkasmukebo7@gmail.com>
2024-01-03 10:19:38 +0000

    Tailwind config screens and color and add directives to your CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/**/*.{html,js}'
+  ],
   theme: {
-    extend: {},
+    screens: {
+      xm: "375px",
+      sm: "480px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1480px",
+    },
+    extend: {
+      colors: {
+        'primary': '#0d121c',
+        'secondary': '#3565a9',
+        'text-color': '#4d5562',
+        'input-color': '#9da3ae',
+      },
+    },
   },
   plugins: [],
 }
